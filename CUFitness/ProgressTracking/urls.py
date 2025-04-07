07ce83ae7d1f8006c31a7ca08f032301e154f9d1
Rat-fi <raphaelleyo@me.com>
2025-04-07 00:44:55 -0400

    frontend-tracking

--- CUFitness/ProgressTracking/urls.py
+++ CUFitness/ProgressTracking/urls.py
@@ -14,10 +14,6 @@ urlpatterns = [
     path('weeklySummary/', views.weekly_summary, name='weekly_summary'),
     path('monthlySummary/', views.monthly_summary, name='monthly_summary'),
     path('historyActivity/', views.history_activity, name='history_activity'),
-    path('weeklyActivityChart/', views.weekly_activity_chart, name='weekly_activity_chart'),
-
-
-
 
     path('progressTracking/', views.index),
     path('checkHistory/<int:id>/', views.checkHistoryData),
